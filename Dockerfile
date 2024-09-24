107bdf0c1b2592c187cace33d02924a9771ce9ae
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-23 22:28:08 -0300

    feat: dokcerfile and update configs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM node:18-alpine
-WORKDIR /backend-lion
-COPY . . 
-RUN npm install --pro
-
-
+FROM node:16
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+COPY .env .env
+RUN npm run build
+EXPOSE 3000
+CMD ["npm", "run", "start:prod"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import { EnterprisesModule } from './enterprises/enterprises.module';
 import { ProjectsModule } from './projects/projects.module';
 import { DependenciesModule } from './dependencies/dependencies.module';
@@ -9,9 +10,14 @@ import { ComponentsModule } from './components/components.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://pedrodev:9j6u2BF2bhshTJuR@crudnest.vzjqsu9.mongodb.net/',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     EnterprisesModule,
     ProjectsModule,
